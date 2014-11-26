c6fd19160c335daaeaffd9b44485ebcc2f334d09
s3rcan <sercanturkmen@outlook.com>
2014-11-26 15:05:17 +0100

    Return activity prototype implemented

--- gen/com/groupd2/doelibs/R.java
+++ gen/com/groupd2/doelibs/R.java
@@ -25,18 +25,34 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int buttonReturnOK=0x7f080001;
+        public static final int buttonReturnReturn=0x7f080006;
+        public static final int buttonReturnScanTag=0x7f080002;
+        public static final int editTextloanableTag=0x7f080000;
+        public static final int layoutReturnLoanInfo=0x7f080003;
+        public static final int textReturnBorrowerName=0x7f080005;
+        public static final int textReturnTitleName=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_return_loanable=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int return_loanable=0x7f070001;
     }
     public static final class string {
+        public static final int OK=0x7f050005;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int currentBorrower=0x7f050008;
         public static final int hello_world=0x7f050001;
+        public static final int loanableTitle=0x7f050007;
+        public static final int loanable_tag=0x7f050004;
+        public static final int returnText=0x7f050009;
+        public static final int scanTag=0x7f050006;
+        public static final int title_activity_return_loanable=0x7f050003;
     }
     public static final class style {
         /** 
@@ -65,5 +81,8 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int generalButton=0x7f060004;
+        public static final int generalEditText=0x7f060003;
+        public static final int generalTextView=0x7f060002;
     }
 }
---
+++ src/com/groupd2/doelibs/ReturnLoanableActivity.java
@@ -0,0 +1,100 @@
+package com.groupd2.doelibs;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class ReturnLoanableActivity extends Activity {
+
+	private EditText tag;
+	private EditText loanableTitle;
+	private EditText currentBorrower;
+	private Button returnButton;
+	private LinearLayout loanableInfo;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_return_loanable);
+		
+		tag = (EditText) findViewById(R.id.editTextloanableTag);
+		loanableTitle = (EditText) findViewById(R.id.textReturnTitleName);
+		currentBorrower = (EditText) findViewById(R.id.textReturnBorrowerName);
+		returnButton = (Button) findViewById(R.id.buttonReturnReturn);
+		loanableInfo = (LinearLayout) findViewById(R.id.layoutReturnLoanInfo);
+		
+		/*tag.setOnEditorActionListener(new OnEditorActionListener() {
+			
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				setVisible(false);
+				return false;
+			}
+		});*/
+		
+		setInfoVisibilty(false);
+	}
+
+	private void setInfoVisibilty(boolean b) {
+		if(b){
+			loanableInfo.setVisibility(LinearLayout.VISIBLE);
+			returnButton.setVisibility(Button.VISIBLE);
+		}else{
+			loanableInfo.setVisibility(LinearLayout.INVISIBLE);
+			returnButton.setVisibility(Button.INVISIBLE);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.return_loanable, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void onOK(View btn){
+		@SuppressWarnings("unused")
+		String tagText = tag.getText().toString(); // can be number as well
+		//do stuff here 
+		
+		//@if valid
+		setInfoVisibilty(true);
+		
+		loanableTitle.setText("Book Name"); //set name here
+		currentBorrower.setText("Borrower Name"); //set name here
+
+		//@else not valid
+		Toast.makeText(this, "Tag is not valid!", Toast.LENGTH_SHORT).show();
+	}
+	
+	public void onScanTag(View btn){
+		setInfoVisibilty(false);
+		//implement scan algorithm here
+		
+		tag.setText("0"); // set text here
+	}
+	
+	public void onReturn(View btn){
+		//return book
+		//make toast
+		//go to somewhere else
+	}
+}

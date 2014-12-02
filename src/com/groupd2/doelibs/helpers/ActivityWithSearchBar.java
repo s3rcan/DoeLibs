a3e744d379210b985caa5760db865605cb8ce6c9
s3rcan <sercanturkmen@outlook.com>
2014-12-02 18:58:46 +0100

    menu implemented

--- src/com/groupd2/doelibs/helpers/ActivityWithSearchBar.java
+++ src/com/groupd2/doelibs/helpers/ActivityWithSearchBar.java
@@ -1,34 +1,67 @@
 package com.groupd2.doelibs.helpers;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.groupd2.doelibs.MyBorrowingsActivity;
 import com.groupd2.doelibs.R;
 
-public class ActivityWithSearchBar extends Activity{
+public class ActivityWithSearchBar extends Activity {
 
 	protected EditText searchBar;
 	protected Button searchButton;
 	protected LinearLayout searchBarLayout;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		searchBar = (EditText) findViewById(R.id.editTextSearchBar);
 		searchButton = (Button) findViewById(R.id.buttonSearch);
 		searchBarLayout = (LinearLayout) findViewById(R.id.layoutSearchBar);
 
 	}
-	
+
 	public void onSearch(View btn) {
-		//perform search
-		
+		// perform search
+
 		Toast.makeText(this, "Search!", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.doelibs_menu, menu);
+		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return onOptionsItemSelected(item, ActivityWithSearchBar.class);
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item, Class<?> cls) {
+		switch (item.getItemId()) {
+		case R.id.menuMyBorrowings:
+			if (cls == MyBorrowingsActivity.class)
+				return true;
+			Intent intent = new Intent(this, MyBorrowingsActivity.class);
+			startActivity(intent);
+			return true;
+		case R.id.menuMyInventory:
+			// TODO redirect to my inventory
+			return true;
+		case R.id.menuLogout:
+			// TODO logout
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
